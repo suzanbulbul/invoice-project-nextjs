d9ddace629bbd2e4645a514432af4bf0d0a1553d
Your Name <you@example.com>
2023-09-27 15:19:04 +0300

    Fix: deploy error

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import Head from 'next/head';
 import { Toaster } from 'react-hot-toast';
 import '../i18n'
+import { useTranslation } from 'react-i18next';
 
 
 //Components
@@ -11,11 +12,15 @@ import Layout from '../components/layout';
 import '../styles/scss/style.scss'
 
 function MyApp({ Component, pageProps }) {
+  const { t } = useTranslation();
 
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap");
     import("@popperjs/core");
   }, []);
+  useEffect(() => {
+  }, [t]);
+  
   
   return (
     <Layout>
